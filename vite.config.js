655acc29744dd8303c10cf0dee11330fb13d11d1
Daniel Popchenko <104201822+DanielPopchenko@users.noreply.github.com>
2025-08-19 15:01:19 -0400

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 // vite.config.ts or vite.config.js
 import { defineConfig } from 'vite';
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
